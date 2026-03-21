c9a08c3a478411a2fa70d9b612f977150c21c4dd
LCA <lca@lca.com>
2026-03-22 00:05:58 +1300

    Auto-migrate KOSH properties on server startup

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,30 @@ if (cnt === 0) {
   console.log('Seed complete.');
 }
 
+// Auto-migrate KOSH properties (insert any missing ones)
+const koshProperties = [
+  '12 Portal Crescent','11 Steele Road','11/182 London Street','1/19 Taylor Terrace',
+  '7/35 Selwyn Street, Tauranga','1/10 Palmerston Street','5/240 Old Farm Road','10 Cook Street',
+  '1/15 Beverley Crescent','6/240 Old Farm Road','74 Awatere Avenue','2/15 Beverley Crescent',
+  '1/3 Glen Lynne Avenue','21 Mistry Place','3/3 Glen Lynne Avenue','16D Ridout Street',
+  '45B Vercoe Road','45A Vercoe Road','11 Raddington Way','11A Raddington Way',
+  '1/120 Beerescourt Road','2/2 Clematis Avenue','22/182 London Street','303/220 Tristram Street',
+  '6A Shirley Place','6 Shirley Place','2/20 Hunter Street','1/22 Willoughby Street',
+  '5/22 Willoughby Street','6/22 Willoughby Street','3/19 Beale Street','1/29 Palmerston Street',
+  '16B Ridout Street','77A Awatere Avenue','163 River Road North','163 River Road South',
+  '7/182 London Street',
+];
+const insertProp = db.prepare('INSERT INTO properties (name, address, airbnb_id) VALUES (?, ?, ?)');
+const existingProps = new Set(db.prepare('SELECT name FROM properties').all().map(r => r.name));
+let migratedCount = 0;
+for (const name of koshProperties) {
+  if (!existingProps.has(name)) {
+    insertProp.run(name, '', '');
+    migratedCount++;
+  }
+}
+if (migratedCount > 0) console.log(`Migrated ${migratedCount} KOSH properties into database.`);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
